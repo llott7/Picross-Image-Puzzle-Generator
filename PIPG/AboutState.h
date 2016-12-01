0358a46b0440f68c24decbd60a2133196909ab77
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2016-12-01 11:23:52 -0600

    AboutState.h
    update

--- PIPG/AboutState.h
+++ PIPG/AboutState.h
@@ -1,8 +1,25 @@
+/*
+	Author: Shaikha Al-Mashaykhi
+	File: AboutState.h
+	Description: The header file for the AboutState.cpp
+
+	Changes log:
+	|Description|			|Date|         |Performed By|
+	
+*/
+
 #pragma once
 class AboutState
 {
 public:
 	AboutState();
 	~AboutState();
+
+private:
+	//sf::Texture aboutStateT;
+	//sf::Sprite aboutStateS;
+
+	//sf::Texture backOptionT;
+	//sf::Texture backOptionS;
 };
 
