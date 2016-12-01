9391f543be27841a38f6338853a3aed9a595e96c
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2016-12-01 11:35:05 -0600

    main.cpp
    added header.. deleted not needed lines of code

--- PIPG/main.cpp
+++ PIPG/main.cpp
@@ -1,3 +1,16 @@
+/*
+	Author: Shaikha Al-Mashaykhi
+	File: main.cpp
+	Description: This file is the main file where it handles rendering the window and the event
+				 for navigating between the states/viewa
+
+	Changes log:
+	|Description|                                                           |Date|         |Performed By|
+	added all the main code needed to rednder a window and polling event  10/20/2016    Shaikha Al-Mashaykhi
+	added the current_State to next_State			              11/30/2016	Lance Lott
+*/
+
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
@@ -22,15 +35,6 @@ void update_main_menu(sf::Event e)
 
 }
 
-void update_scan_screen(sf::Event e) 
-{
-	
-}
-
-void update_create_screen(sf::Event e) 
-{
-
-}
 
 int main()
 {
@@ -49,12 +53,6 @@ int main()
 	//window.clear();
 
 	window.display();
-
-	
-	//ScanState scanS;
-	
-	//optionA.getTextureRect();
-	//optionB.getTextureRect();
 
 	//opening and closing window event
 	while (window.isOpen())
@@ -106,11 +104,6 @@ int main()
 
 		//std::cout << sf::Mouse::getPosition(window).x << std::endl;
 		//sf::Mouse::setPosition(sf::Vector2i(window.getSize().x /2, window.getSize().y/2), window);
-
-		
-		//time = clock.getElapsedTime();
-		//scanS.update(time.asSeconds());
-		//clock.restart().asSeconds();
 
 		
 	}
