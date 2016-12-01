9df0f5326bff0b4b9801d2133fdf9bb172ce5d94
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2016-12-01 11:40:29 -0600

    SolveState.cpp
    Added the header and the code for setting the positions of the sprites

--- PIPG/SolveState.cpp
+++ PIPG/SolveState.cpp
@@ -1,3 +1,18 @@
+/*
+	Author: Shaikha Al-Mashaykhi
+	File: SolveState.cpp
+	Description: This file controls loading all the graphics for the
+				Solve State/view. Also, sitting the right positions for
+				each sprite.
+
+	Changes log:
+	|Description|                                       |Date|         |Performed By|
+	Loading the images, setting textures and Sprites  11/01/2016    Shaikha Al-Mashaykhi
+	Added the draw function                           11/01/2016	Shaikha Al-Mashaykhis
+	added the set positiong for each sprite		  11/30/2016	Shaikha Al-Mashaykhi
+	added the scanState::action			  11/30/2016	Lance Lott
+*/
+
 #include "SolveState.h"
 
 using namespace std;
@@ -40,6 +55,14 @@ SolveState::SolveState(sf::RenderWindow &window):State(window)
 	titleS.setTexture(titleT);
 	solveItForMeOptionS.setTexture(solveItForMeOptionT);
 	backOptionS.setTexture(backOptionT);
+	
+	//set the title in the top left of the window
+	titleS.setPosition(5.f, 5.f);
+
+	//set position of scan option image
+	solveItForMeOptionS.setPosition(window.getSize().x / 10.5, window.getSize().y / 1.2);
+	backOptionS.setPosition(window.getSize().x / 10.5, window.getSize().y / 1.1);
+	giveMeHintOptionS.setPosition(window.getSize().x / 1.3, window.getSize().y / 1.2);
 
 }
 
