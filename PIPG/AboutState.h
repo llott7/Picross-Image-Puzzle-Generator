5472b1800d93d270c61d91ea56f5c50bd340b82d
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2016-12-01 14:20:49 -0600

    AboutState.h
    Update

--- PIPG/AboutState.h
+++ PIPG/AboutState.h
@@ -4,22 +4,37 @@
 	Description: The header file for the AboutState.cpp
 
 	Changes log:
-	|Description|			|Date|         |Performed By|
-	
+	|Description|									|Date|         |Performed By|
+	added all the main code needed to be implemented in the CreateState.cpp file  10/20/2016    Shaikha Al-Mashaykhi
 */
 
 #pragma once
-class AboutState
+
+#ifndef ABOUT_STATE
+#define ABOUT_STATE
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include <cstdlib>
+#include "State.h"
+
+class AboutState:public State
 {
 public:
-	AboutState();
+	AboutState(sf::RenderWindow &window);
 	~AboutState();
+	void draw(sf::RenderWindow &window);
+	std::string action(sf::RenderWindow &window);
+
 
 private:
-	//sf::Texture aboutStateT;
-	//sf::Sprite aboutStateS;
+	sf::Texture aboutStateT;
+	sf::Sprite aboutStateS;
 
-	//sf::Texture backOptionT;
-	//sf::Texture backOptionS;
+	sf::Texture blueSquareT;
+	sf::Sprite blueSquareS;
+
+	sf::Texture backOptionT;
+	sf::Sprite backOptionS;
 };
 
+#endif
